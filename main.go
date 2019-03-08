4e54d95fd4dc401a6381028c95c7246747c07c37
jleyba <xleyba@gmail.com>
2019-03-08 13:00:49 +0100

    Added server parameter and gracefull dead

--- main.go
+++ main.go
@@ -11,14 +11,16 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
+	"flag"
+	"context"
+	"os/signal"
 )
-
 
 // Configuration
 type Configuration struct {
 	Port string
 }
-
 
 // Convert String to int
 func StrToInt(str string) (int, error) {
@@ -102,20 +104,27 @@ func factorialRecursiveHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+
+
 func main() {
 
+	// Used for gracefull server shutdown
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second * 15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
+	// Config data
 	port := ":9596"
 	log.Println("Starting server")
 
 	// Start to read conf file
-	log.Println("calculator v0.5")
+	log.Println("\n\n")
 	log.Println("=============================================")
-	log.Println("         Configuration checking")
+	log.Println("   Configuration checking - calculator v0.6")
 	log.Println("=============================================")
 	file, err := os.Open("conf.json")
 
-
-	if (err != nil) {
+	if err != nil {
 		log.Println("No conf file, using port 9596 by default")
 	} else {
 		defer file.Close()
@@ -141,14 +150,61 @@ func main() {
 
 	log.Println("=============================================")
 
+
 	router := mux.NewRouter() //.StrictSlash(true)
+
 	router.HandleFunc("/", Index).Methods("GET")
 	router.HandleFunc("/echo/{message}", echoHandler).Methods("GET")
 	router.HandleFunc("/factorialIterative/{number}", factorialIterativeHandler).Methods("GET")
 	router.HandleFunc("/factorialRecursive/{number}", factorialRecursiveHandler).Methods("GET")
 
-	log.Println("Running server....")
+	// set timeout
+	muxWithMiddlewares := http.TimeoutHandler(router, time.Second*3, "Timeout!")
 
-	log.Fatal(http.ListenAndServe(port, router))
+	srv := &http.Server{
+		Addr:    port,
+		// Good practice to set timeouts to avoid Slowloris attacks.
+		// Using just the read parameter due to this article
+		// https://stackoverflow.com/questions/29334407/creating-an-idle-timeout-in-go
+		//WriteTimeout: time.Second * 60,
+		ReadTimeout:  time.Second * 15,
+		////:  time.Second * 120,
+		//Handler: router,
+		Handler: muxWithMiddlewares,
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		log.Println("Running server....")
+
+		//log.Fatal(http.ListenAndServe(port, router))
+		//log.Println(s.ListenAndServe())
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	fmt.Println("\n\n")
+	log.Println("shutting down")
+	log.Println("Goddbye!....")
+	os.Exit(0)
+
 
 }
